4471210654fcdcd35e406042991ca02d9ce98509
hshamsadd <hshamsadd@gmail.com>
2025-05-14 20:01:49 +0200

    Update Spread-Notes.js

--- Week-3/3-Spread/Spread-Notes.js
+++ Week-3/3-Spread/Spread-Notes.js
@@ -1,3 +1,4 @@
+```javascript
 const cephalopods = [
   "dumbo octopus",
   "humboldt squid",
@@ -19,6 +20,7 @@ console.log(mollusca);
 const inverts = [...cnidaria, ...gastropods, ...cephalopods];
 console.log(inverts);
 //["fire coral", "moon jelly", "giant african snail", "banana slug", "variable neon slug", "dumbo octopus", "humboldt squid", "flamboyant cuttlefish"]
+```
 
 const cephCopy = [...cephalopods];
 console.log(cephCopy);
